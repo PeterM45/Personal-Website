04ac05c016f84f6665388ae239ba25059b31004b
PeterM45 <petermang122@gmail.com>
2022-01-25 16:18:54 -0500

    add about me and projects

--- js/script.js
+++ js/script.js
@@ -14,3 +14,37 @@ function Clock() {
     hours + "  :  " + minutes + "  :  " + seconds + " " + amPm;
   var t = setTimeout(Clock, 500);
 }
+
+setTimeout(function () {
+  document.querySelector(".about-me-section").classList.remove("hide");
+  document.querySelector(".projects-section").classList.remove("hide");
+  document
+    .querySelector(".about-me-section")
+    .scrollIntoView({ behavior: "smooth", block: "center" });
+}, 7000);
+
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+  threshold: 0.3,
+  rootMargin: "0px 0px 100px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
